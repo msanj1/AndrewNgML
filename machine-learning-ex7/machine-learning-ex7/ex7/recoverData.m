d0b1141929e7833fa29468d85b8b84e87f24a834
Mohsen S <mohsen.sanjari@outlook.com>
2020-03-09 08:20:26 +1000

    implemented the Recover data portion of the exercise

--- machine-learning-ex7/machine-learning-ex7/ex7/recoverData.m
+++ machine-learning-ex7/machine-learning-ex7/ex7/recoverData.m
@@ -21,7 +21,9 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
+u_reduce = U(:,1:K);
 
+X_rec = Z * u_reduce'; %m * 1  1 * 2
 
 % =============================================================
 
